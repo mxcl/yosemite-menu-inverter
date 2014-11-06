86cd66799801ff60c507ac10623cc927f6ca7870
Maxime Thirouin <m@moox.io>
2014-11-06 05:48:38 +0100

    F.lux don't need it anymore

--- invert.rb
+++ invert.rb
@@ -75,20 +75,6 @@ if File.directory? "/Applications/Degrees.app"
   relaunch "Degrees"
 end
 
-# F.lux
-if File.directory? "/Applications/Flux.app"
-  %w{flux-icon-mono4 geoloc-arrow}.each do |suffix|
-    prefix = "/Applications/Flux.app/Contents/Resources/"
-    img = "#{prefix}#{suffix}.tiff"
-    system "convert -negate #{img} #{img}"
-
-    if not $?.success?
-      abort "    try: brew install imagemagick --with-libtiff"
-    end
-  end
-  relaunch "Flux"
-end
-
 # GrabBox
 if File.directory? "/Applications/GrabBox.app"
   %w{menuicon-animation-1 menuicon-animation-2 menuicon-animation-3 menuicon-animation-4 menuicon-animation-5 menuicon-animation-6 menuicon-animation-7 menuicon-animation-8 menuicon}.each do |suffix|
